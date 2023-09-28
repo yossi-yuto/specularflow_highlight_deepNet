8a28c17a34cff647de44ba9b525fe81cf11ca1ad
yossi-yuto <yoshimura@g.wakayama-u.jp>
2023-09-28 21:38:18 +0900

    upload tmp2_pipeline.sh

---
+++ tmp2_pipeline_mold.sh
@@ -0,0 +1,13 @@
+# GPU setting number ex) 0
+DEVICE_NUM=$1
+# execute date, ex) 20230912
+date=$2
+kind=$3
+iter=$4
+write_dir=${date}/${kind}
+CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/plastic_mold_dataset -test_mold_type ${kind} -mode rccl -write_dir ${write_dir}/ex_${iter} --train
+CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/plastic_mold_dataset -test_mold_type ${kind} -mode ssf -write_dir ${write_dir}/ex_${iter} --train 
+CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/plastic_mold_dataset -test_mold_type ${kind} -mode sh -write_dir ${write_dir}/ex_${iter} --train
+CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/plastic_mold_dataset -test_mold_type ${kind} -mode refine -write_dir ${write_dir}/ex_${iter} --train --eval
+CUDA_VISIBLE_DEVICES=${DEVICE_NUM} python main.py -dataset_path data/plastic_mold_dataset -test_mold_type ${kind} -mode pmd -write_dir ${write_dir}/ex_${iter} --train --eval
+
